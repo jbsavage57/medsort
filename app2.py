5c2ed507374c350aac77baf19f4e8725bfdfd76b
jbsavage57 <65708078+jbsavage57@users.noreply.github.com>
2022-11-22 16:11:16 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ from flask import Flask
  
 # Flask constructor takes the name of
 # current module (__name__) as argument.
-app = Flask(__name__)
+app2 = Flask(__name__)
  
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
@@ -19,4 +19,4 @@ if __name__ == '__main__':
  
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run()
+    app2.run()
